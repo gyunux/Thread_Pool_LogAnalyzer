65371d8a9aa3917beeab5c451946f499a669f8cf
gyunux <67477254+gyunux@users.noreply.github.com>
2025-08-20 17:16:50 +0900

    feat: 생산자-소비자 패턴 로그 분석기 구현

--- src/main/java/com/nhnacademy/thread_pool/Consumer.java
+++ src/main/java/com/nhnacademy/thread_pool/Consumer.java
@@ -14,21 +14,36 @@ public class Consumer extends Thread{
     @Override
     public void run(){
         while(true){
-            String line = null;
+            String line;
             try {
                 line = logQueue.getLog();
+                // Producer가 보내는 종료 신호(Poison Pill)를 확인합니다.
+                if ("END_OF_LOGS".equals(line)) {
+                    break;
+                }
             } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-            if(line == null){
+                // 스레드가 중단되면, 중단 상태를 다시 설정하고 루프를 종료합니다.
+                Thread.currentThread().interrupt();
                 break;
             }
-            String[] splitLine = line.split(" ");
-            if(splitLine[2].equals("INFO")){
-                logCounter.plusInfoCount();
-            } else if(splitLine[2].equals("WARN")){
-                logCounter.plusWarnCount();
-            } else{
+
+            try {
+                int firstSpace = line.indexOf(' ');
+                int secondSpace = line.indexOf(' ', firstSpace + 1);
+
+                // 로그 레벨의 시작 문자를 확인하여 카운트를 증가시킵니다.
+                if (secondSpace > 0) {
+                    char logLevelChar = line.charAt(secondSpace + 1);
+                    if (logLevelChar == 'I') { // INFO
+                        logCounter.plusInfoCount();
+                    } else if (logLevelChar == 'W') { // WARN
+                        logCounter.plusWarnCount();
+                    } else { // ERROR
+                        logCounter.plusErrorCount();
+                    }
+                }
+            } catch (StringIndexOutOfBoundsException e) {
+                // 잘못된 형식의 로그 라인은 ERROR로 카운트합니다.
                 logCounter.plusErrorCount();
             }
         }
